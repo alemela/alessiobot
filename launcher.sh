1c62a097db8dfcd7d7f3a30bd50e43a479db5a1b
alemela <alessiom92@gmail.com>
2014-12-24 12:08:34 +0100

    Added avvisi e some fixes

--- launcher.sh
+++ launcher.sh
@@ -17,32 +17,33 @@ echo "["$(date +%Y%m%d%H%M%S)"] Starting launcher!"
 
 count=1
 while read line           
-do     
-case $count in
-    1)      
-        name=$line
-		;;
-	2)
-        list_path=$line${date}.txt
-		;;
-	3)
-        py_path=$line
-		;;
-	4)
-        log_path=$line	
-		;;
-	5)
-        echo "["$(date +%Y%m%d%H%M%S)"] Generating list for ${name}..."
-        ./lists.sh "$name" "$list_path"
+do
+if [[ ! $line == \#* ]]; then
+    case $count in
+        1)
+            name=$line
+		    ;;
+	    2)
+            list_path=$line${date}.txt
+		    ;;
+	    3)
+            py_path=$line
+		    ;;
+	    4)
+            log_path=$line
+		    ;;
+	    5)
+            echo "["$(date +%Y%m%d%H%M%S)"] Generating list for ${name}..."
+            ./lists.sh "$name" "$list_path"
 
-        echo "["$(date +%Y%m%d%H%M%S)"] Launching python script for ${name}..."
-        python $py_path $name >> $log_path 2>&1 &
+            echo "["$(date +%Y%m%d%H%M%S)"] Launching python script for ${name}..."
+            python $py_path $name >> $log_path 2>&1 &
 		
-		count=0
-		;;
-esac
-let count++
-
+		    count=0
+		    ;;
+    esac
+    let count++
+fi
 done < $1
 
 #####
